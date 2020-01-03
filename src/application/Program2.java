e165c8e73efb3d10dac1bc92722dd188d810ee1b
Leonardo Sbarai <leonardo.sbarai@dextra-sw.com>
2020-01-02 22:04:22 -0300

    DepartmentDao delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("===== TEST 1: department findById =====");
@@ -32,5 +35,12 @@ public class Program2 {
         department.setName("Games");
         departmentDao.update(department);
         System.out.println("Updated completed!");
+
+        System.out.println("\n===== TEST 5: department delete =====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed.");
+        sc.close();
     }
 }
