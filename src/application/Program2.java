ff769c3c188f45d27c9e0c697854875c71faa91c
Leonardo Sbarai <leonardo.sbarai@dextra-sw.com>
2020-01-02 21:56:55 -0300

    DepartmentDao insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -3,21 +3,28 @@ package application;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program2 {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("===== TEST 1: seller findById =====");
+        System.out.println("===== TEST 1: department findById =====");
         Department department = departmentDao.findById(1);
         System.out.println(department);
 
-        System.out.println("\n===== TEST 3: seller findByAll =====");
+        System.out.println("\n===== TEST 2: department findByAll =====");
         List<Department> list = departmentDao.findAll();
         for (Department obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n===== TEST 3: department insert =====");
+        Department newDepartment = new Department(null, "Toys");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -54,7 +54,6 @@ public class SellerDaoJDBC implements SellerDao {
             throw new DbException(e.getMessage());
         } finally {
             DB.closeStatement(st);
-
         }
 
     }
